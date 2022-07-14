4979bc619d49c3ce4843070a0a01a46de3cfec48
Dhuuvviinn <108947549+Dhuuvviinn@users.noreply.github.com>
2022-07-14 12:29:55 +0530

    Update Navbar.js

--- src/Component/navbar/Navbar.js
+++ src/Component/navbar/Navbar.js
@@ -17,7 +17,7 @@ function Navbar() {
             <a href="#home">Home</a>
           </p>
           <p>
-            <a href="#wgpt3">What is GPT</a>
+            <a href="#wwhgpt3">What is GPT</a>
           </p>
           <p>
             <a href="#possibility">OpenApi</a>
@@ -51,7 +51,7 @@ function Navbar() {
                 <a href="#home">Home</a>
               </p>
               <p>
-                <a href="#wgpt3">What is GPT</a>
+                <a href="#wwhgpt3">What is GPT</a>
               </p>
               <p>
                 <a href="#possibility">OpenApi</a>
